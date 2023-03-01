2b351facc29967cdd9875e365c2bae9d57ea6dd6
star-39 <star3nine@mail.ru>
2023-03-01 11:55:11 +0900

    Update to v0.1.8

--- lib/src/define.dart
+++ lib/src/define.dart
@@ -1,5 +1,5 @@
 const appName = "Moe Sticker App";
-const versionNumber = "v0.1.7";
+const versionNumber = "v0.1.8";
 const projectUrl = "https://github.com/star-39/moe-sticker-bot";
 const webappUrl = String.fromEnvironment('MSB_WEBAPP_URL');
 const botName = String.fromEnvironment('MSB_BOT_NAME');
